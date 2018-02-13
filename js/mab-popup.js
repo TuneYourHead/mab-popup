d94769926ddec1bfd2ad5d3ad0a5fde2338d3305
Tanya <tuneyourhead@gmail.com>
2018-02-13 16:19:55 +0200

    made js prettier

--- js/mab-popup.js
+++ js/mab-popup.js
@@ -1,28 +1,35 @@
 $( document ).ready(function(){
+
   $("body").prepend("<div class='darkOverlay' style='display: none'></div>");
   var actionButton = $('button').filter("[data-pop]");
+
   actionButton.on("click", function(){
+
     var currentActionButton = $(this);
     var targetId = '#' + ($(this).attr("data-pop"));
     var AnimationIn = ($(""+targetId+"").attr("data-pop-animation"));
     var AnimationOut = ($(""+targetId+"").attr("data-pop-animation-out"));
+
     var targetAnimationIn = function(){
+      $(""+targetId+"").removeClass("animated");
       $(""+targetId+"").addClass(""+AnimationIn+"");
       $(""+targetId+"").addClass("animated");
       setTimeout(function(){
         $(""+targetId+"").removeClass(""+AnimationIn+"");
       }, 1000);
     }
+
     var targetAnimationOut = function(){
-      setTimeout(function(){
-        $(""+targetId+"").removeClass(""+AnimationOut+"");
-        $(""+targetId+"").css("display", "none");
-      }, 1000);
       $(""+targetId+"").removeClass("animated");
       $(""+targetId+"").addClass(""+AnimationOut+"");
       $(""+targetId+"").addClass("animated");
       $(".darkOverlay").fadeOut(1000);
+      setTimeout(function(){
+        $(""+targetId+"").removeClass(""+AnimationOut+"");
+        $(""+targetId+"").css("display", "none");
+      }, 1000);
     }
+
     if($(""+targetId+"").is(':hidden')){
       if($(""+targetId+"").hasClass("darkOverlayOn")){
         $(".darkOverlay").fadeIn(500);
@@ -30,7 +37,6 @@ $( document ).ready(function(){
       if($(this).filter("[data-pop-animation]")){
         targetAnimationIn();
       }
-      $(""+targetId+"").show();
     }else{
       if($(this).attr("data-pop-out")){
         if($(this).filter("[data-pop-animation-out]")){
@@ -38,8 +44,10 @@ $( document ).ready(function(){
         }
       }
     }
+
     $(".darkOverlay").on("click", function(){
       targetAnimationOut();
     });
+    
   });
 })
